4dfb3083b3f51eb3b201c40d90b2b31d55e87806
Alexander Bikadorov <bikaejkb@mailbox.tu-berlin.de>
2016-06-03 19:53:37 +0200

    fileKey: comparing additionally with lazy md5sum of file
    Getting into pros and cons of this gibberish language.

--- src/main/scala/FileKey.scala
+++ src/main/scala/FileKey.scala
@@ -1,18 +1,26 @@
+/*
+ * DupFinder
+ * Copyright (C) 2016 A.B.
+ */
+
 import java.io.File
 
 /**
   * Utility key object that is equal for equal files. Immutable.
+  *
+  * @author Alexander Bikadorov { @literal <bikaejkb@mail.tu-berlin.de>}
   */
-class FileKey(file: File) {
-  private val file_ = file
+class FileKey(val file: File) {
+  private lazy val hash = Utils.md5sum(file)
 
   override def equals(that: Any): Boolean =
     that match {
-      case that: FileKey => that.file_.length() == file_.length()
+      // no element comparison with "==" for Scala "Array"s
+      case that: FileKey => file.length() == that.file.length() && hash.sameElements(that.hash)
       case _ => false
     }
 
-  override def hashCode: Int = 31 + 7 * file_.length().toInt
+  override def hashCode: Int = 31 + 7 * file.length().toInt
 
-  override def toString: String = file_ + "#" + hashCode
+  override def toString: String = file + "#" + hashCode
 }
